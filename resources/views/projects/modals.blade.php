661c85ae400de3384f4a31fdc966485a631c1ec3
Dmitriy Bavykin <1996@3g.ua>
2017-08-05 16:18:41 +0300

    separating js for initializing binds keys after auth and registration

--- resources/views/projects/modals.blade.php
+++ resources/views/projects/modals.blade.php
@@ -1,3 +1,4 @@
 @include('projects.creating_project_block')
 @include('projects.updating-project-block')
 @include('projects.updating_task_block')
+<script src="{{ asset('js/projectslist.js') }}"></script>

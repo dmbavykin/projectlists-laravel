c50bbc900e18e2e17a749fc2045f051d26fe4501
Dmitriy Bavykin <1996@3g.ua>
2017-08-15 11:51:08 +0300

    styling

--- resources/views/projects/auth_response.blade.php
+++ resources/views/projects/auth_response.blade.php
@@ -1,6 +1,7 @@
+<h1 class="text-center">TODO lists</h1>
 @foreach($projects as $project)
     <div class="container-fluid tl-block project{{ $project['id'] }}">
-        <div class="container">
+        <div class="container tl-head-block">
             <div class="tl-head">
                 <input type="hidden" name="project" value="{{ $project['id'] }}">
                 <div class="col-md-1 text-center"><i class="glyphicon glyphicon-calendar"></i></div>
@@ -12,7 +13,7 @@
         <div class="container">
             <div class="add-task-block">
                 <div class="col-md-1 text-center"><i class="glyphicon glyphicon-plus"></i></div>
-                <div class="col-md-8"><input type="text" name="content" class="form-control"></div>
+                <div class="col-md-8"><input type="text" name="content" class="form-control task-adding"></div>
                 <div class="col-md-3">
                     <button class="btn btn-default add-task">Add Task</button>
                 </div>
--- resources/views/projects/new_task.blade.php
+++ resources/views/projects/new_task.blade.php
@@ -1,8 +1,9 @@
 <div class="task task{{ $task['id'] }}">
     <input type="hidden" name="task_id" value="{{ $task['id'] }}">
     <input type="hidden" name="order" value="{{ $task['order'] }}">
-    <div class="col-md-1">
-        <input type="checkbox" class="is-done" @if($task['is_done']) checked @endif>
+    <div class="col-md-1 text-center">
+        <input type="checkbox" id="done-status{{ $task['id'] . $task['order'] }}" class="is-done" @if($task['is_done']) checked @endif>
+        <label for="done-status{{ $task['id'] . $task['order'] }}"></label>
     </div>
     <div class="col-md-6 task-content">{{ $task['content'] }}</div>
     <div class="col-md-1 ordering text-center">
